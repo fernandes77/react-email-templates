6c963bdf415419c1279c0824b8d4442ee1a3ad0b
Rodrigo Fernandes <me@rodrigofernand.es>
2024-07-24 17:23:29 -0300

    welcome

---
+++ emails/welcome.tsx
@@ -0,0 +1,145 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+
+interface ResetPasswordProps {
+  name: string;
+}
+
+const baseUrl = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : "";
+
+export const ResetPasswordEmail = ({ name }: ResetPasswordProps) => (
+  <Html>
+    <Head />
+    <Preview>Welcome to RET</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Section style={box}>
+          <Img
+            src={`${baseUrl}/static/ret.png`}
+            width="36"
+            height="36"
+            alt="React Email Template"
+          />
+          <Heading style={title}>Hi {name}, welcome to RET</Heading>
+          <Text style={description}>
+            Congratulations! You're joining over 5 trillion developers around
+            the world who use RET to develop emails faster and in a familiar
+            language.
+          </Text>
+          <Text style={description}>
+            You can view your templates and a variety of other information about
+            your account right from your dashboard.
+          </Text>
+          <Section style={buttonSection}>
+            <Button style={button} href="https://example.com">
+              View your dashboard
+            </Button>
+          </Section>
+          <Text style={{ ...description, marginTop: "12px" }}>
+            If you haven't started yet, you might find our{" "}
+            <Link style={link} href="https://example.com">
+              docs
+            </Link>{" "}
+            handy.
+          </Text>
+          <Text style={signOff}>
+            Best,
+            <br />
+            RET Team
+          </Text>
+          <Hr />
+          <Text style={footer}>
+            React Email Templates, Suite 425 59431 Allen View, East Sherell, MT
+            81789
+          </Text>
+        </Section>
+      </Container>
+    </Body>
+  </Html>
+);
+
+ResetPasswordEmail.PreviewProps = {
+  name: "Alan",
+} as ResetPasswordProps;
+
+export default ResetPasswordEmail;
+
+const main: React.CSSProperties = {
+  backgroundColor: "#fafafa",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Ubuntu,sans-serif',
+};
+
+const container: React.CSSProperties = {
+  backgroundColor: "#ffffff",
+  margin: "0 auto",
+  borderRadius: "8px",
+  border: "1px solid #e4e4e7",
+};
+
+const box: React.CSSProperties = {
+  padding: "24px 24px 16px 24px",
+};
+
+const title: React.CSSProperties = {
+  color: "#09090b",
+  fontSize: "24px",
+  fontWeight: "bold",
+  padding: "0",
+  marginTop: "32px",
+};
+
+const description: React.CSSProperties = {
+  marginTop: "8px",
+  lineHeight: "28px",
+  fontSize: "16px",
+};
+
+const buttonSection: React.CSSProperties = {
+  textAlign: "center",
+  marginTop: "24px",
+};
+
+const button: React.CSSProperties = {
+  display: "inline-flex",
+  justifyContent: "center",
+  alignItems: "center",
+  whiteSpace: "nowrap",
+  borderRadius: "6px",
+  fontSize: "14px",
+  fontWeight: "600",
+  backgroundColor: "#18181B",
+  color: "#fafafa",
+  padding: "12px 16px",
+};
+
+const link: React.CSSProperties = {
+  color: "#09090b",
+  textDecoration: "underline",
+  textUnderlineOffset: "4px",
+  fontWeight: 600,
+};
+
+const signOff: React.CSSProperties = { color: "#71717a", marginTop: "16px" };
+
+const footer: React.CSSProperties = {
+  color: "#71717a",
+  fontSize: "14px",
+  lineHeight: "20px",
+  marginTop: "24px",
+};
