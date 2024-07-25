63e21fb1b20591169b227925d0d27055c390c21d
Rodrigo Fernandes <me@rodrigofernand.es>
2024-07-25 11:32:08 -0300

    add box-shadow

--- emails/assignment.tsx
+++ emails/assignment.tsx
@@ -92,6 +92,8 @@ const container: React.CSSProperties = {
   margin: "0 auto",
   borderRadius: "8px",
   border: "1px solid #e4e4e7",
+  boxShadow:
+    "rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 1px 2px 0px",
 };
 
 const box: React.CSSProperties = {
